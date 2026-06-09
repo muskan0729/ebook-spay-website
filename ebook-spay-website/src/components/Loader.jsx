d35034998e37c9daee995eaa792092cf57cf107d
Intern-003 <intern@spay.live>
2026-06-09 13:44:50 +0530

    Delete ebook-spay-website directory

--- ebook-spay-website/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loader() {
-  return (
- <div className="flex items-center justify-center py-70">
-      <div className="w-16 h-16 border-4 border-[#ed2c64] border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  )
-}
